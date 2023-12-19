b9d3a243a8863e3bff9005db9c9c9b2c58f6ba31
Lautaro Jayat <lautaro.jayat@gmail.com>
2023-12-18 21:03:39 -0300

    feat: moving to dark mode

--- ruok-ui/src/main.tsx
+++ ruok-ui/src/main.tsx
@@ -7,7 +7,16 @@ import App from './App.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <CssVarsProvider>
+    <CssVarsProvider
+      defaultMode="dark" // the selector to apply CSS theme variables stylesheet.
+      colorSchemeSelector="#dark-mode-by-default"
+      //
+      // the local storage key to use
+      modeStorageKey="dark-mode-by-default"
+      //
+      // set as root provider
+      //disableNestedContext
+    >
       <CssBaseline />
       <Router>
         <App />
